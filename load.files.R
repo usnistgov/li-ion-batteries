0560c58299e6f3c73991ce84bc0727e2c8f1a05a
apprenticeGee <stanley.gilbert@nist.gov>
2025-08-20 15:42:27 -0400

    Add error processing to file loading

--- load.files.R
+++ load.files.R
@@ -1,10 +1,17 @@
 load.data <- function(final=NULL, data=NULL, dir){
   # Either load the completed data (in `final`)
   # or the relevant base data (in `data`).
-  files <- final
   ff <- list.files(dir) %>% grep("RData", ., ignore.case=T, value=T)
-  if(! all(tolower(files) %in% tolower(ff))) files <- data
+
+  if(all(tolower(final) %in% tolower(ff))){
+    files <- final
+  } else if(all(tolower(data) %in% tolower(ff))){
+    files <- data
+  } else {
+    return(1)
+  }
   for(i in tolower(files)){
     i %>% match(tolower(ff)) %>% { ff[.] } %>% paste0(dir, .) %>% load(envir=.GlobalEnv)
   }
+  return(0)
 }
